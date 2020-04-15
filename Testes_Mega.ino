dc9165b1bf4489b6f68e37ce5b5482bcd7a5207d
Leandro Serra Acosta <leandrosly@gmail.com>
2020-04-14 22:16:50 -0300

    Formatação do float melhorada
    Usando dtostrf() pra converter o float pra string e formatando a partir dele.

--- Testes_Mega.ino
+++ Testes_Mega.ino
@@ -1,9 +1,9 @@
-#include <LiquidCrystal.h>
+#include "LiquidCrystal.h"
 #include "DHT.h"
 #include "RTClib.h"
 
 LiquidCrystal lcd(53,52,48,49,50,51);
-char linha1[16], linha2[16];
+char linha1[17], linha2[17];
 
 RTC_DS1307 rtc;
 char dow[7][4] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
@@ -13,31 +13,27 @@ char mon[12][4] = {"Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep"
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-int chk;
-float hum, temp;
+char hum[5], temp[5];
 
 void setup() {
   Serial.begin(9600);
-  
+
   lcd.begin(16,2);
-  
+
   dht.begin();
-  
+
   if (!rtc.begin()) {
-    Serial.println("rtc not working");
+    Serial.println("RTC not working!");
     while(true);
   } else {
-    Serial.println("rtc connected");
+    Serial.println("RTC connected");
   }
-  
+
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop() {
   delay(2000);
-
-  float hum = dht.readHumidity();
-  float temp = dht.readTemperature();
 
   DateTime now = rtc.now();
 
@@ -46,19 +42,12 @@ void loop() {
   sprintf(linha1, "%02u:%02u %3s %3s/%02u", now.hour(), now.minute(), dow[now.dayOfTheWeek()], mon[now.month()-1], now.day());
   lcd.print(linha1);
   lcd.setCursor(0,1);
-  sprintf(linha2, " %s    %2.2f ", "teste", hum);
+  dtostrf(dht.readTemperature(), 4, 1, temp);
+  dtostrf(dht.readHumidity(), 4, 1, hum);
+  sprintf(linha2, " %s%cC   %s%% ", temp, char(223), hum);
   lcd.print(linha2);
-  //lcd.print(temp);
-  //lcd.print(char(223));
-  //lcd.print("C   61.2% ");
 
   Serial.println(linha1);
-
-  Serial.print("Hum: ");
-  Serial.print(hum);
-  Serial.print(" %, Temp: ");
-  Serial.print(temp);
-  //Serial.print("\xC2\xB0");
-  //Serial.println("C");
+  Serial.println(linha2);
   delay(5000);
 }
